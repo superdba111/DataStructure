4f13599a96064373f725fc20649f2f4a37a1fc39
Yingyou Li <31944577+superdba111@users.noreply.github.com>
2020-07-08 15:27:41 -0500

    Add files via upload

--- checkLeapYear.py
+++ checkLeapYear.py
@@ -2,11 +2,5 @@
 year = int(input("Enter Year: "))
 
 # Leap Year Check
-if year % 4 == 0 and year % 100 != 0:
-    print(year, "is a Leap Year")
-elif year % 100 == 0:
-    print(year, "is not a Leap Year")
-elif year % 400 ==0:
-    print(year, "is a Leap Year")
-else:
-    print(year, "is not a Leap Year")
+def is_leap(year):
+   return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
